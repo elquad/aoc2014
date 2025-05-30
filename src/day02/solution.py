c5051c1cf6e3f0e4f257c0d2c9dc3720f8018d8d
Filip Škola <filip@fqs.cz>
2025-05-30 14:03:27 +0200

    Add day 2 solution

---
+++ src/day02/solution.py
@@ -0,0 +1,72 @@
+from collections import deque
+
+
+class SlidingPairs:
+    """Iterates over elements from line, optionally skips an element."""
+    def _advance_window(self) -> None:
+        if self._skip_idx is not None and self._skip_idx == self._index:
+            next(self._source)
+        self._queue.append(next(self._source))
+        self._index += 1
+
+    def __init__(self, input_seq: str, skip_idx: int | None = None):
+        self._skip_idx = skip_idx
+        self._index = 0
+        self._source = (int(x) for x in input_seq.split())
+        self._queue = deque()
+        self._stopped = False
+        for x in range(2):
+            try:
+                self._advance_window()
+            except StopIteration:
+                raise ValueError("Input sequence too short")
+
+    def __iter__(self) -> 'SlidingPairs':
+        return self
+
+    def __next__(self) -> tuple[int, int]:
+        if self._stopped:
+            raise StopIteration
+        res = tuple(self._queue)
+        self._queue.popleft()
+        try:
+            self._advance_window()
+        except StopIteration:
+            self._stopped = True
+        return res
+
+
+def is_pair_safe(diff: int, first_diff: int) -> bool:
+    if diff == 0 or abs(diff) > 3:
+        return False
+    if diff > 0 and first_diff < 0:
+        return False
+    if diff < 0 and first_diff > 0:
+        return False
+    return True
+
+
+def is_sequence_safe(line: str, skip_idx: int | None = None) -> bool:
+    first_diff = None
+    for idx, (a, b) in enumerate(SlidingPairs(line, skip_idx)):
+        if first_diff is None:
+            first_diff = a - b
+        if not is_pair_safe(a - b, first_diff):
+            if skip_idx is not None:
+                return False  # second pass permits no unsafe pairs
+            return any([
+                    is_sequence_safe(line, idx-1),
+                    is_sequence_safe(line, idx),
+                    is_sequence_safe(line, idx+1)
+            ])
+
+    return True
+
+
+def get_safe_count(filename: str) -> int:
+    with open(filename) as f:
+        return sum(is_sequence_safe(line) for line in f)
+
+
+report_safe = get_safe_count("../../inputs/day02.txt")
+print(report_safe)
