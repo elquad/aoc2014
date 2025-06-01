b38468903553a530e1b55ddff19d37a8e0ecc0f1
Filip Škola <filip@fqs.cz>
2025-06-01 15:30:55 +0200

    Add day 03, second part

--- src/day03/solution.py
+++ src/day03/solution.py
@@ -1,13 +1,17 @@
 import re
 
 
+pattern = r"mul\((-?\d+),(-?\d+)\)"
+enablers = r"do\(\)"
+disablers = r"don't\(\)"
+
+
 def read_file(filename):
     with open(filename) as f:
         return f.read()
 
 
 def match_patterns(content):
-    pattern = r"mul\((-?\d+),(-?\d+)\)"
     matches = re.findall(pattern, content)
     return matches
 
@@ -16,6 +20,40 @@ def compute_muls(matches):
     return sum(int(a) * int(b) for a, b in matches)
 
 
-content = read_file("../../inputs/day03.txt")
-patterns = match_patterns(content)
-print(compute_muls(patterns))
+def find_and_compute_muls(pattern, content):
+    res = re.finditer(pattern, content)
+    return {m.start(): int(m.group(1)) * int(m.group(2)) for m in res}
+
+
+def find_instructions(content):
+    res1 = re.finditer(enablers, content)
+    res2 = re.finditer(disablers, content)
+    return {m.start(): "enable" for m in res1} | {m.start(): "disable" for m in res2}
+
+
+def get_instructed_muls(muls: dict[int, int], instrs: dict[int, str]) -> int:
+    d = muls | instrs
+    res = 0
+    disabled = False
+    for key in sorted(d):
+        val = d[key]
+        if val == "enable":
+            disabled = False
+            continue
+        if disabled:
+            continue
+        if val == "disable":
+            disabled = True
+            continue
+        res += d[key]
+    return res
+
+
+if __name__ == "__main__":
+    content = read_file("../../inputs/day03.txt")
+    patterns = match_patterns(content)
+    print(compute_muls(patterns))  # Part 1
+
+    muls = find_and_compute_muls(pattern, content)
+    instrs = find_instructions(content)
+    print(get_instructed_muls(muls, instrs))  # Part 2
