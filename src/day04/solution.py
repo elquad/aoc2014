a828043d34358d2886f920906e7c6a2675cf352b
Filip Škola <filip@fqs.cz>
2025-06-02 08:23:24 +0200

    Improvements in diagonal search, bound testing fix

--- src/day04/solution.py
+++ src/day04/solution.py
@@ -1,6 +1,6 @@
 """
 AoC 2024, day 04, part 1
-First sketch of the XMAS word search puzzle.
+Find horizontal, vertical, and diagonal occurences of WORD in the input.
 """
 
 
@@ -9,6 +9,7 @@ import re
 
 
 WORD = "XMAS"
+DIAGONALS = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
 
 
 def count_diagonal_patterns(y: int, x: int, text: list[str]) -> int:
@@ -16,23 +17,18 @@ def count_diagonal_patterns(y: int, x: int, text: list[str]) -> int:
     Finds count of the four possible diagonal patterns,
     makes sure negative slicing does not detect false positives.
     """
-    ops = {'+': operator.add, '-': operator.sub}
     count = 0
-    for op1 in ops.keys():
-        if op1 == '+' and y > len(text) - len(WORD) + 1:
+    for dy, dx in DIAGONALS:
+        if dy == 1 and y > len(text) - len(WORD):
             continue
-        if op1 == '-' and y < len(WORD) - 1:
+        if dy == -1 and y < len(WORD) - 1:
             continue
-        for op2 in ops.keys():
-            if op2 == '+' and x > len(text[0]) - len(WORD) + 1:
-                continue
-            if op2 == '-' and x < len(WORD) - 1:
-                continue
-            try:
-                if all(text[ops[op1](y, i)][ops[op2](x, i)] == WORD[i] for i in range(1, len(WORD))):
-                    count += 1
-            except IndexError:
-                pass
+        if dx == 1 and x > len(text[0]) - len(WORD):
+            continue
+        if dx == -1 and x < len(WORD) - 1:
+            continue
+        if all(text[y+dy*i][x+dx*i] == WORD[i] for i in range(1, len(WORD))):
+            count += 1
     return count
 
 
@@ -42,11 +38,11 @@ def find_patterns(text: list[str]) -> int:
     diagonal = 0
 
     for line in zip(*text):
-        vertical += len(re.findall(WORD, ''.join(line)))
-        vertical += len(re.findall(WORD[::-1], ''.join(line)))
+        vertical += ''.join(line).count(WORD)
+        vertical += ''.join(line[::-1]).count(WORD)
     for line_idx, line in enumerate(text):
-        horizontal += len(re.findall(WORD, line))
-        horizontal += len(re.findall(WORD[::-1], line))
+        horizontal += line.count(WORD)
+        horizontal += line.count(WORD[::-1])
         for char_idx, char in enumerate(line):
             if char == WORD[0]:
                 diagonal += count_diagonal_patterns(line_idx, char_idx, text)
@@ -56,4 +52,4 @@ def find_patterns(text: list[str]) -> int:
 
 if __name__ == "__main__":
     with open("../../inputs/day04.txt") as f:
-        print(find_patterns([line.strip() for line in f.readlines()]))
+        print(find_patterns([line.strip() for line in f.readlines()]))  # Part 01
